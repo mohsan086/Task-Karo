8cef8165757ecce0d92e54505459aaf20f5f4bc5
Mohsan Nawab <117937671+mohsan086@users.noreply.github.com>
2025-08-23 14:37:51 +0500

    task-card.tsx

--- components/task-card.tsx
+++ components/task-card.tsx
@@ -161,7 +161,7 @@ export function TaskCard({ task }: TaskCardProps) {
               <Button
                 variant="default"
                 size="sm"
-                className="h-8 w-8 p-0 opacity-0 group-hover:opacity-100 transition-opacity"
+                className="h-8 w-8 p-0 "
               >
                 <MoreHorizontal className="w-4 h-4" />
               </Button>
